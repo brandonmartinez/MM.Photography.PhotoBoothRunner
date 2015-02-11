120033986620554e0aa7a5264ce62bb5d383904e
Brandon Martinez <me@brandonmartinez.com>
2015-02-11 04:37:04 +0000

    Converting to node

---
+++ server.js
@@ -0,0 +1,10 @@
+/*
+ * PhotoBoothRunner - Server
+ * https://github.com/brandonmartinez/MM.Photography.PhotoBoothRunner
+ *
+ * Copyright (c) 2015 Martinez Media, LLC
+ * Licensed under the MIT license.
+ */
+
+var PhotoBoothRunner = require('./lib/PhotoBoothRunner.js');
+PhotoBoothRunner.start();
